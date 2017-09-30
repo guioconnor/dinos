6e3bc3a0d382e37f5f29659846270eeceb6a8304
Guilherme O'Connor <guilherme@fundapps.co>
2017-09-30 10:22:59 +0100

    Renamed Roar component

---
+++ src/components/atoms/Roar/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Roar = () => (
+  <audio className="player" preload autoPlay>
+    <source src="https://raw.githubusercontent.com/guioconnor/dinos/master/src/components/atoms/Roar/TyrannosaurusRexRoar.mp3" />
+  </audio>
+);
+
+export default Roar;
